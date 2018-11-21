2f5fd0efd7194c33e945b926ea2136a430e3b7dc
Dhananjay <dhananjaymohnot@gmail.com>
2018-11-21 13:10:47 +0530

    Hide data in environment variables, BuildConfig and Android Studio.

--- app/src/main/java/hide/sensitive/info/in/android/app/MainActivity.kt
+++ app/src/main/java/hide/sensitive/info/in/android/app/MainActivity.kt
@@ -1,12 +1,15 @@
 package hide.sensitive.info.`in`.android.app
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dataTextView.text = getString(R.string.valueFromEnvironmentVariable, BuildConfig.Base_URL)
     }
 }
